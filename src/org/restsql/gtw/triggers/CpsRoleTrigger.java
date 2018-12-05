a40df8740bf062f724be86af793f6c3924056553
mlarese <mauro.larese@gmail.com>
2018-12-05 21:35:23 +0100

    update

--- src/org/restsql/gtw/triggers/CpsRoleTrigger.java
+++ src/org/restsql/gtw/triggers/CpsRoleTrigger.java
@@ -23,8 +23,14 @@ public class CpsRoleTrigger extends AbstractTrigger {
         if(httpServletRequest.isUserInRole("reporting_cp")) {
             if(httpServletRequest.getUserPrincipal()!=null) {
                 String userName = httpServletRequest.getUserPrincipal().getName();
+                // cpuser_1
                 System.out.println("------ u="+ userName);
-                RequestValue rqv = new RequestValue("cp_name", userName, RequestValue.Operator.Equals);
+
+                String[] as = userName.split("_");
+                Integer is = new Integer(as[1]);
+
+                // RequestValue rqv = new RequestValue("cp_name", userName, RequestValue.Operator.Equals);
+                RequestValue rqv = new RequestValue("cp_id", userName, RequestValue.Operator.Equals);
                 request.getParameters().add(rqv);
             }
         }
@@ -32,5 +38,12 @@ public class CpsRoleTrigger extends AbstractTrigger {
 
         System.out.println("---------- trigger "+res);
 
+    }
+
+    public static void main(String[] args) {
+        String s = "cpuser_2";
+        String[] as = s.split("_");
+        Integer is = new Integer(as[1]);
+        System.out.println(is +100);
     }
 }
